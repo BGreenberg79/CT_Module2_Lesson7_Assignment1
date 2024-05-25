c413760823c560386ee347d48b25e011983bb46b
BGreenberg79 <greenbeb2@gmail.com>
2024-05-25 01:33:08 -0400

    1st commit

---
+++ 1_exceptional_weather_forecast.py
@@ -0,0 +1,21 @@
+#Task 1 Start
+
+def temp_conversion(input_temp):
+    try:
+        converted_temp = int(input_temp)
+        return converted_temp
+    except ValueError:
+        print("Please enter a valid integer or float for the temperature input")
+
+while True:
+    temp_list = []
+    user_temp = input("Please enter a temperature in Fahrenheit (when finished type 'done'): ")
+    if user_temp == "done":
+        break
+    else:
+        processed_temp = temp_conversion(user_temp)
+        temp_list = temp_list.append(processed_temp)
+        
+print("The temperatures added are: ")
+for item in temp_list:
+    print(f"{item} degrees Fahrenheit")
---
+++ simple.py
@@ -0,0 +1,3 @@
+error_ex = input("enter a number: ")
+error_ex = int(error_ex)
+print(error_ex)
